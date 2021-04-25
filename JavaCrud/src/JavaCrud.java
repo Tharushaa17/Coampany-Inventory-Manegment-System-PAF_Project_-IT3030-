48490d19c8a37e9a0fdcfc23d4a2bec021d7c005
Naveen <nimsara.shakith@gmail.com>
2021-04-25 08:05:57 +0530

    my Part

---
+++ JavaCrud/src/JavaCrud.java
@@ -0,0 +1,134 @@
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JPanel;
+import javax.swing.border.TitledBorder;
+import javax.swing.JTextField;
+import javax.swing.JTable;
+import javax.swing.JScrollPane;
+
+public class JavaCrud {
+
+	private JFrame frame;
+	private JTextField textpname;
+	private JTextField textField;
+	private JTextField textField_1;
+	private final JTable table = new JTable();
+	private JTextField textField_2;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					JavaCrud window = new JavaCrud();
+					window.frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public JavaCrud() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Shopping Cart");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lblNewLabel.setBounds(130, 0, 266, 37);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JPanel panel = new JPanel();
+		panel.setBorder(new TitledBorder(null, "Add Cart", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel.setBounds(12, 38, 265, 110);
+		frame.getContentPane().add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("Product Name");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_1.setBounds(12, 24, 100, 16);
+		panel.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_1_1 = new JLabel("Edition");
+		lblNewLabel_1_1.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_1_1.setBounds(12, 53, 88, 16);
+		panel.add(lblNewLabel_1_1);
+		
+		JLabel lblNewLabel_1_2 = new JLabel("Price");
+		lblNewLabel_1_2.setFont(new Font("Tahoma", Font.BOLD, 13));
+		lblNewLabel_1_2.setBounds(12, 82, 88, 16);
+		panel.add(lblNewLabel_1_2);
+		
+		textpname = new JTextField();
+		textpname.setBounds(112, 21, 138, 22);
+		panel.add(textpname);
+		textpname.setColumns(10);
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setBounds(112, 79, 138, 22);
+		panel.add(textField);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(112, 50, 138, 22);
+		panel.add(textField_1);
+		
+		JButton btnNewButton = new JButton("Save");
+		btnNewButton.setBounds(22, 145, 77, 25);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnExit = new JButton("Exit");
+		btnExit.setBounds(111, 145, 77, 25);
+		frame.getContentPane().add(btnExit);
+		
+		JButton btnClear = new JButton("Clear");
+		btnClear.setBounds(200, 145, 77, 25);
+		frame.getContentPane().add(btnClear);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(289, 45, 131, 161);
+		frame.getContentPane().add(scrollPane);
+		scrollPane.setViewportView(table);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new TitledBorder(null, "Search", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		panel_1.setBounds(12, 174, 242, 66);
+		frame.getContentPane().add(panel_1);
+		
+		JLabel lblNewLabel_1_1_1 = new JLabel("Product ID");
+		lblNewLabel_1_1_1.setFont(new Font("Tahoma", Font.BOLD, 13));
+		panel_1.add(lblNewLabel_1_1_1);
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		panel_1.add(textField_2);
+		
+		JButton btnUpdate = new JButton("Update");
+		btnUpdate.setBounds(266, 219, 77, 25);
+		frame.getContentPane().add(btnUpdate);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.setBounds(343, 219, 77, 25);
+		frame.getContentPane().add(btnDelete);
+	}
+}
